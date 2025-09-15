d347da726ec9f0381bd57f2938f5896fe6353b3c
Ayush Pathak <ayush.pathak@fitpass.co.in>
2025-09-15 22:50:38 +0530

    first array question search element in an array

---
+++ Array/searchElementArray.js
@@ -0,0 +1,21 @@
+// write a function that search for an element in an array, return the index if element is in present, if element is not present return -1?
+
+function searchElementInArray(array, element) {
+  const length = array.length;
+
+  if (length === 0) {
+    console.log("Array is empty!");
+  }
+
+  for (let i = 0; i < length; i++) {
+    const value = array[i];
+    if (value === element) {
+      console.log("element found in an array,", i);
+      return element;
+    }
+  }
+
+  console.log("Element not present in an array, ", -1);
+}
+
+searchElementInArray([1, 4, 5, 3, 6, 2, 7, 8, 9, 4, 5, 7], 7);
